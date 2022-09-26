bc43ab0f3c77c3642cb0de94637698ba15df742b
Tiago Silva <tiagoluizrs@gmail.com>
2022-09-26 09:55:33 -0300

    Primeiro commit na product

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 const Product = () => {
     const params = useParams();
 
-    return <h1>Produto {params.id}</h1>
+    return <h1>Produto {params.id} branch catalogo</h1>
 }
 
 export default Product;
