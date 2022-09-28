b810082720892755b3b170691a210672b4755581
Tiago Silva <tiagoluizrs@gmail.com>
2022-09-28 09:31:27 -0300

    Conclusão do produto

--- src/pages/catalog/catalog.jsx
+++ src/pages/catalog/catalog.jsx
@@ -14,7 +14,7 @@ const Catalog = () => {
             Object.keys(products).map(id => {
                 return <Grid item xs={12} sm={6} md={4} lg={3} className="catalog-item">
                     <span className="badge-item">10%</span>
-                    <img src={products[id].image}/>
+                    <img src={products[id].images[0]}/>
 
                     {
                         products[id].categories.map(categoryId => {
@@ -23,6 +23,16 @@ const Catalog = () => {
                     }
 
                     <Typography variant="h5" component="h2">{products[id].name}</Typography>
+
+                    {
+                      products[id].promo_price ? <Typography variant="p" component="p" className="promo_price">{products[id].promo_price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</Typography> : ""
+                    }
+                    <Typography variant="p" component="p" className="price" style={{
+                        fontSize: products[id].promo_price ? '16px' : '20px',
+                        color: products[id].promo_price ? '#a9a9a9' : '#333333',
+                        textDecoration: products[id].promo_price ? 'line-through' : 'none',
+                    }}>{products[id].price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</Typography>
+
                     <Typography variant="p" component="p">{products[id].description.substring(0, 100)}...</Typography>
                     <Link to={"../product/" + id}>
                         <Button fullWidth variant="contained">
